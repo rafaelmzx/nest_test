d1967f64ee415eaaafe0c04e4c7f76190f1fc7b4
Rafael Moura <rafaelleduard@gmail.com>
2022-10-03 22:26:22 -0300

    Config / Add TypeORM

--- migrations/V1_create_table_user.sql
+++ migrations/V1_create_table_user.sql
@@ -4,5 +4,14 @@ CREATE TABLE IF NOT EXISTS usuarios
   nome varchar(255) NOT NULL,
   ativo BOOLEAN NOT NULL,
   criado_em TIMESTAMP,
-  atuailizado_em TIMESTAMP
+  atualizado_em TIMESTAMP
+);
+
+INSERT INTO usuarios  
+(
+	id, nome, ativo, criado_em, atualizado_em
+)
+VALUES
+(
+	1, 'Rafael', true, NOW()::timestamp, NOW()::timestamp
 );
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigModule } from './appconfig.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [    
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [],
+    synchronize: false,
+    ssl: {
+      rejectUnauthorized: false,
+    }
+  }),
+  AppConfigModule,
+  UsersModule,
+],
   controllers: [AppController],
   providers: [AppService],
 })
